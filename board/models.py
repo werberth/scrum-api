d2ae5d73fdd573621bcf447e6f35d9a0ac2c9642
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-22 15:55:59 -0300

    Adding sprint model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Sprint(models.Model):
+    """Development interation period"""
+
+    name = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField(blank=True, default='')
+    end = models.DateField(unique=True)
+
+    def __str__(self):
+        return self.name or _('Sprint ending %s') % self.end
