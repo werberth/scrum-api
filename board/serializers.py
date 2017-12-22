4d26ccdb1bafc39bdea657d06698c118897c48c9
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-22 17:43:49 -0300

    Customizing information view of some TaskSerializer fields

--- board/serializers.py
+++ board/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
 from .models import Sprint, Task
+
+User = get_user_model()
 
 
 class SprintSerializer(serializers.ModelSerializer):
@@ -11,12 +14,19 @@ class SprintSerializer(serializers.ModelSerializer):
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    status_display = serializers.SerializerMethodField('get_status_display')
+    assigned = serializers.SlugRelatedField(
+        slug_field=User.USERNAME_FIELD, required=False
+    )
 
     class Meta:
         model = Task
         fields = (
             'id', 'name',
             'description', 'sprint',
-            'status', 'order', 'assigned',
-            'started', 'due', 'completed',
+            'status', 'order', 'status_display',
+            'assigned', 'started', 'due', 'completed',
         )
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
