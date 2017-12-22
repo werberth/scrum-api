b68c5f3f388b39cad41c94807d6d24d8de5d5c7c
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-22 14:22:09 -0300

    Removing django admin urls

--- scrum/urls.py
+++ scrum/urls.py
@@ -1,10 +1,4 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import include, url
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'scrum.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+]
