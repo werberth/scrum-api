8684aac46151e05d166d0877125e6435299ea880
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-22 17:07:43 -0300

    Adding DefaultsMixin and implementing in SprintViewSet

--- board/views.py
+++ board/views.py
@@ -1,10 +1,30 @@
-from rest_framework import viewsets
+from rest_framework import (
+    authentication,
+    permissions,
+    viewsets
+)
 
 from .models import Sprint
 from .serializers import SprintSerializer
 
 
-class SprintViewSet(viewsets.ModelViewSet):
+class DefaultsMixin(object):
+    """Default settings for view authentication permissions,
+    filtering and pagination."""
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """API endpoint for listing and creating sprints"""
 
     queryset = Sprint.objects.order_by('end')
