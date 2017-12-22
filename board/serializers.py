8666f98fb55d9be8e12554e177b7621d8c5e1778
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-22 17:20:10 -0300

    Adding TaskSerializer

--- board/serializers.py
+++ board/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Sprint
+from .models import Sprint, Task
 
 
 class SprintSerializer(serializers.ModelSerializer):
@@ -8,3 +8,15 @@ class SprintSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sprint
         fields = ('id', 'name', 'description', 'end',)
+
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Task
+        fields = (
+            'id', 'name',
+            'description', 'sprint',
+            'status', 'order', 'assigned',
+            'started', 'due', 'completed',
+        )
