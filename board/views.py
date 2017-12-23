f604347794f4ce6dbf49d783395d9d01a1536401
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-22 22:54:44 -0300

    Adding TaskViewSet and UserViewSet

--- board/views.py
+++ board/views.py
@@ -1,11 +1,15 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import (
     authentication,
     permissions,
     viewsets
 )
 
-from .models import Sprint
-from .serializers import SprintSerializer
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+
+User = get_user_model()
 
 
 class DefaultsMixin(object):
@@ -29,3 +33,22 @@ class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+
+
+class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating tasks."""
+
+    queryset = Task.objects.all()
+    serializers_class = TaskSerializer
+
+
+class UserViewSet(
+    DefaultsMixin,
+    viewsets.ReadOnlyModelViewSet
+):
+    """API endpoit for listing users."""
+
+    lookup_field = User.USERNAME_FIELD
+    lookup_field_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
