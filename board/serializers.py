faf1fd77b9084be6b8bde0a73c51c20e9ab89d67
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-23 00:45:12 -0300

    Adding Model Task reference link on the TaskSerializer and Model Sprint reference on SprintSerializer

--- board/serializers.py
+++ board/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from django.contrib.auth import get_user_model
 
 from .models import Sprint, Task
@@ -22,9 +23,20 @@ class UserSerializer(serializers.ModelSerializer):
 
 class SprintSerializer(serializers.ModelSerializer):
 
+    links = serializers.SerializerMethodField()
+
     class Meta:
         model = Sprint
-        fields = ('id', 'name', 'description', 'end',)
+        fields = ('id', 'name', 'description', 'end', 'link')
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse(
+                'sprint-detail',
+                kwargs={'pk': obj.pk}, request=request,
+            )
+        }
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -33,11 +45,12 @@ class TaskSerializer(serializers.ModelSerializer):
         slug_field=User.USERNAME_FIELD, required=False,
         queryset=User.objects.all()
     )
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = Task
         fields = (
-            'id', 'name',
+            'id', 'name', 'links',
             'description', 'sprint',
             'status', 'order', 'status_display',
             'assigned', 'started', 'due', 'completed',
@@ -45,3 +58,12 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def get_status_display(self, obj):
         return obj.get_status_display()
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse(
+                'task-detail',
+                kwargs={'pk': obj.pk}, request=request,
+            )
+        }
