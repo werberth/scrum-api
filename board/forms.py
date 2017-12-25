3d1211e23ac147d89968fae6472abe151c1f1a4d
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-24 21:24:40 -0300

    Adding backlog filter to TaskFilter

--- board/forms.py
+++ board/forms.py
@@ -3,8 +3,19 @@ import django_filters
 from .models import Task
 
 
+class NullFilter(django_filters.BooleanFilter):
+    """Filter on a field set as null or not."""
+
+    def filter(self, qs, value):
+        if value is not None:
+            return qs.filter(**{'{}__isnull'.format(self.name): value})
+        return qs
+
+
 class TaskFilter(django_filters.FilterSet):
+
+    backlog = NullFilter(name='sprint')
 
     class Meta:
         model = Task
-        fields = ('sprint', 'status', 'assigned',)
+        fields = ('sprint', 'status', 'assigned', 'backlog',)
