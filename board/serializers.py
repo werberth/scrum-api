18a3653de7bc74bb352dbb70fddda6c6009593f8
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-25 23:15:55 -0300

    Adding task list link to SprintSerializer

--- board/serializers.py
+++ board/serializers.py
@@ -46,7 +46,11 @@ class SprintSerializer(serializers.ModelSerializer):
             'self': reverse(
                 'sprint-detail',
                 kwargs={'pk': obj.pk}, request=request,
-            )
+            ),
+            'tasks': reverse(
+                'task-list',
+                request=request
+            ) + '?sprint={}'.format(obj.pk)
         }
 
 
